d35ebc57dcb0677dc9bc8f7c73d9d28f6a2c8e98
Berzok <34751752+Berzok@users.noreply.github.com>
2018-05-24 12:05:18 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
-#include "source.h"
-
+#include"fonctions.h"
+//#include<sqlca.h>
 
 int main()
 	{
-	char utilisateur[2];
-	connexion();
+	char tamere;
+	menu(connexion());
 	return 0;
 	}
+	
+	
+	
+#ifndef FONCTIONS_H_INCLUDED
+#define FONCTIONS_H_INCLUDED
+#define pass (void)0
+
+
+//void affichage(char requete[2]){
+//	char debutR = requete[0];
+//	char forR = requete[1];
+//	char whereR = requete[2]
+//	EXEC SQL DECLARE C CURSOR FOR debutR FROM forR whereR;
+//	EXEC SQL OPEN C;
+//	EXEC SQL FETCH C INTO
+
+
+//}
+
+
+
+
+
+
+
+
+
+#endif // FONCTIONS_H_INCLUDED
